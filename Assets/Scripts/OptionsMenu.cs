4f17bbe79156a03da9d85bd74941f85095583470
ZoeMRuf <92019231+ZoeMRuf@users.noreply.github.com>
2023-01-22 20:54:58 +0100

    template change with options menu

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 public class OptionsMenu : MonoBehaviour
 {
     public Slider mainSlider;
+    public Dropdown templates;
 
     // Start is called before the first frame update
     void Start()
     {
         //Adds a listener to the main slider and invokes a method when the value changes.
         mainSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        templates.onValueChanged.AddListener(delegate { TemplateChangeCheck(); });
+
+        
     }
 
     // Update is called once per frame
@@ -23,8 +27,13 @@ public class OptionsMenu : MonoBehaviour
     // Invoked when the value of the slider changes.
     public void ValueChangeCheck()
     {
-        print(mainSlider.value);
         AudioListener.volume = mainSlider.value;
+    }
+
+    public void TemplateChangeCheck()
+    {
+        print(templates.value);
+        MainMenu.template = templates.value;
     }
 
 }
